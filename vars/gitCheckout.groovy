578a2c77a1d868e14473580cb7a9b82c2fdeb744
shantanux.com <shantanudeshmu50@gmail.com>
2024-01-08 18:57:45 +0530

    [][][]

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams){
     checkout([
         $class: 'gitSCM',
         branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [( url: stageParams.url)]
+        userRemoteConfigs: [[url: stageParams.url]]
             ])
 }
 
